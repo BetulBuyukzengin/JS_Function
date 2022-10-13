17b847683d3e96823ea421bcf0d3892d98b898c3
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2022-10-14 00:10:28 +0300

    Update function.js

--- function.js
+++ function.js
@@ -23,10 +23,37 @@ var animal=cat=>`Hi, ${cat}`;
 console.log(animal("Siamese"))
 
 /****************  ES5  *********************/
+
 function operation(x,y){
     return x*y;
 }
 console.log(operation(2,4))
+
 /***************** ES6 **********************/
+
 var operation=(x,y)=>"x*y: "+ x*y;
 console.log(operation(2,5))
+
+/****************************************************/
+
+const movieList=list=>{
+    list.forEach((movie,index)=> {
+        console.log(`${index+1}.${movie}`)
+    });
+};
+console.log(movieList(["Firefly","The Mandalorian","Breaking Bad"]));
+
+/*********************************************************************/
+
+const numList=(numbers)=>{
+    const newNumbers=numbers.map(e=>{
+        if(e%2==0){
+            return e*2
+        }
+        else if(e%2==1){
+            return e*3
+        }
+    });
+    return newNumbers
+}
+console.log(numList([1,2,3,4,5,6]));
