8e4a52633805e5ea6475d49bab5e08c171645db2
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2022-10-13 17:24:51 +0300

    Update function.js

--- function.js
+++ function.js
@@ -6,7 +6,7 @@ function person(girl, man){
 console.log(person("Betül","Kadir"))
 
 /*******************************************/
-        // ES5
+
 var person=function(name,surname){
     return `Your's name: ${name} 
 Your's surname: ${surname}`;
@@ -14,10 +14,19 @@ Your's surname: ${surname}`;
 console.log(person("Aycan","Yılmaz"))
 
 /*******************************************/
-//     Fat Arrow Function (Arrow Function) - ES6
+//     Fat Arrow Function (Arrow Function) ES6
 
 var person=(girl,man)=>`Hello, ${girl} and ${man}`;
 console.log(person("Seda","Mert"))
 
 var animal=cat=>`Hi, ${cat}`;
 console.log(animal("Siamese"))
+
+/****************  ES5  *********************/
+function operation(x,y){
+    return x*y;
+}
+console.log(operation(2,4))
+/***************** ES6 **********************/
+var operation=(x,y)=>"x*y: "+ x*y;
+console.log(operation(2,5))
